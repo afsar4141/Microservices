7ff765134e686d686e1e1a37cd2c2ff0b1ea1dc9
afsar4141 <afsaralikhan4141@gmail.com>
2026-07-31 15:29:13 +0530

    first commit

---
+++ auth/src/routes/auth.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const validators = require('../middlewares/validator.middleware');
+const authController = require("../controllers/auth.controller")
+const authMiddleware = require('../middlewares/auth.middleware');
+
+const router = express.Router();
+
+router.post('/register', validators.registerUserValidations, authController.registerUser);
+
+router.post('/login', validators.loginUserValidations, authController.loginUser);
+
+router.get('/me', authMiddleware.authMiddleware, authController.getCurrentUser);
+
+router.get("/logout", authController.logoutUser);
+
+router.get('/users/me/addresses', authMiddleware.authMiddleware, authController.getUserAddresses);
+
+router.post("/users/me/addresses", validators.addUserAddressValidations, authMiddleware.authMiddleware, authController.addUserAddress)
+
+router.delete("/users/me/addresses/:addressId", authMiddleware.authMiddleware, authController.deleteUserAddress)
+
+module.exports = router;
